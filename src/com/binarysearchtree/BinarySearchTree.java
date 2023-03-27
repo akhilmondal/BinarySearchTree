d5571ebc4c1abd62ea2aea74961ae59608ee49b2
akhilmondal <akhilmandalfamily@gmail.com>
2023-03-27 06:14:37 +0530

    Added: program to search a element of BST.

--- src/com/binarysearchtree/BinarySearchTree.java
+++ src/com/binarysearchtree/BinarySearchTree.java
@@ -3,26 +3,26 @@ package com.binarysearchtree;
 public class BinarySearchTree {
     private TreeNode root;
 
-    private class TreeNode{
+    private class TreeNode {
         private int data;
         private TreeNode left;
         private TreeNode right;
 
-        public TreeNode(int data){
+        public TreeNode(int data) {
             this.data = data;
         }
     }
 
-    public void insert(int value){
+    public void insert(int value) {
         root = insert(root, value);
     }
 
-    public TreeNode insert( TreeNode root, int value){
-        if (root == null || root.data == value){
+    public TreeNode insert(TreeNode root, int value) {
+        if (root == null || root.data == value) {
             root = new TreeNode(value);
             return root;
         }
-        if (value < root.data){
+        if (value < root.data) {
             root.left = insert(root.left, value);
         } else {
             root.right = insert(root.right, value);
@@ -32,13 +32,14 @@ public class BinarySearchTree {
 
     //Here we are using inorder method to traverse through the node and showing the data.
     public void inOrder(TreeNode root) {
-        if(root == null) {
+        if (root == null) {
             return;
         }
         inOrder(root.left);
         System.out.print(root.data + " ");
         inOrder(root.right);
     }
+
     public int size() {
         return size(root);
     }
@@ -50,16 +51,33 @@ public class BinarySearchTree {
             return size(node.left) + 1 + size(node.right);
         }
     }
-        public static void main(String[] args) {
-            System.out.println("Welcome to Binary Search Tree. ");
-            BinarySearchTree tree = new BinarySearchTree();
-            tree.insert(57);
-            tree.insert(30);
-            tree.insert(70);
-            tree.inOrder(tree.root);
-            int size = tree.size();
-            System.out.println(" ");
-            System.out.println("Size of the Binary Search Tree is: " +size);
 
+    public TreeNode search(TreeNode root, int key) {
+        if (root == null || root.data == key) {
+            return root;
+        }
+        if (key < root.data) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Binary Search Tree. ");
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(57);
+        tree.insert(30);
+        tree.insert(70);
+        tree.inOrder(tree.root);
+        int size = tree.size();
+        System.out.println(" ");
+        System.out.println("Size of the Binary Search Tree is: " + size);
+        System.out.println("Searching ::::::::::::::::");
+        TreeNode temp = tree.search(tree.root, 63);
+        if (temp == null)
+            System.out.println("The data isn't Found.");
+        else
+            System.out.println("The data is Found: " +temp.data);
     }
 }
