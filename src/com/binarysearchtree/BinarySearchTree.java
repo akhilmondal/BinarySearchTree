7d65f2ed3bf87dd25b84ed7f6bf47f61c388407f
akhilmondal <akhilmandalfamily@gmail.com>
2023-03-27 05:49:58 +0530

    Added: program for a BST is added to the branch.

--- src/com/binarysearchtree/BinarySearchTree.java
+++ src/com/binarysearchtree/BinarySearchTree.java
@@ -1,8 +1,52 @@
 package com.binarysearchtree;
 
 public class BinarySearchTree {
+    private TreeNode root;
+
+    private class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    public void insert(int value){
+        root = insert(root, value);
+    }
+
+    public TreeNode insert( TreeNode root, int value){
+        if (root == null || root.data == value){
+            root = new TreeNode(value);
+            return root;
+        }
+        if (value < root.data){
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    //Here we are using inorder method to traverse through the node and showing the data.
+    public void inOrder(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
 
         public static void main(String[] args) {
             System.out.println("Welcome to Binary Search Tree. ");
+            BinarySearchTree tree = new BinarySearchTree();
+            tree.insert(57);
+            tree.insert(30);
+            tree.insert(70);
+            tree.inOrder(tree.root);
+
     }
 }
